d042d131c94563b8a96cca9761fbd818a9022ca5
awsomedod <awsomedod@gmail.com>
2025-08-18 16:06:54 -0700

    requirements

--- app.py
+++ app.py
@@ -243,5 +243,4 @@ def update_sources_sync():
     return jsonify({'new_sources': filtered_sources}), 200
 
 if __name__ == "__main__":
-    port = 8080
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=8080)
